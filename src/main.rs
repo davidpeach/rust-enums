3765ce85633147d46c016fdc1315e5e146c1ce6a
David Peach <mail@davidpeach.co.uk>
2020-06-14 10:53:27 +0100

    Refactor to more concise enums with data attached directly.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,10 @@
-// Define an Enum for known enumerations of a type
-enum IpAddrKind {
-    V4,
-    V6,
-}
-
-// Define a struct to group an IP Address with it's type
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+enum IpAddr {
+    V4(String),
+    V6(String),
 }
 
 fn main() {
+    let home = IpAddr::V4(String::from("127.0.0.1"));
 
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    let loopback = IpAddr::V6(String::from("::1"));
 }
